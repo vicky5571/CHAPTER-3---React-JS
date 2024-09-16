1c87a1acdee8f675de8761b21c1eb0716cb9e215
vicky5571 <vickygp5571@gmail.com>
2024-09-16 18:57:51 +0700

    Separated each component to independent file

---
+++ 4. Shopping Notes/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer({ items }) {
+  if (!items.length) return <footer className="stats">Daftar belanja kosong!</footer>;
+  const totalItems = items.length;
+  const checkedItems = items.filter((item) => item.checked).length;
+  const percentage = Math.round((checkedItems / totalItems) * 100);
+  return (
+    <footer className="stats">
+      Ada {totalItems} barang di daftar belanjaan, {checkedItems} barang sudah dibeli ({percentage}%)
+    </footer>
+  );
+}
