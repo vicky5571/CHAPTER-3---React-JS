06cac13b8cebfe6d4e34536c8da32435a2beaad3
vicky5571 <vickygp5571@gmail.com>
2024-09-23 12:09:23 +0700

    onSubmit event

--- 2. (Anson Developer) React JS Full Course 2024/src/App.jsx
+++ 2. (Anson Developer) React JS Full Course 2024/src/App.jsx
@@ -1,31 +1,9 @@
-export default function App() {
-  // NOT_VERIFIED / VERIFIED / ACCOUNT_DISABLED
-  const USER_STATUS = "VERIFIED";
+import { LoginForm } from "./components/LoginForm";
 
-  switch (USER_STATUS) {
-    case "NOT_VERIFIED":
-      return (
-        <div>
-          <span>Please verify your email or mobile number.</span>
-        </div>
-      );
-    case "VERIFIED":
-      return (
-        <div>
-          <span>Click here to access your dashboard.</span>
-        </div>
-      );
-    case "ACCOUNT_DISABLED":
-      return (
-        <div>
-          <span>Your account is disabled.</span>
-        </div>
-      );
-    default:
-      return (
-        <div>
-          <span>Please contact the admin</span>
-        </div>
-      );
-  }
+export default function App() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
 }
---
+++ 2. (Anson Developer) React JS Full Course 2024/src/components/LoginForm.jsx
@@ -0,0 +1,44 @@
+export function LoginForm() {
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const username = formData.get("username");
+        const password = formData.get("password");
+        fetch("localhost:5173/api/login"),
+          {
+            body: {
+              username,
+              password,
+            },
+            method: "POST",
+          };
+      }}
+    >
+      <label htmlFor="username">Username</label>
+      <br />
+      <input
+        id="username"
+        type="text"
+        onChange={(e) => {
+          console.log(`Username: ${e.target.value}`);
+        }}
+        name="username"
+      />
+      <br />
+      <label htmlFor="password">Password</label>
+      <br />
+      <input
+        id="password"
+        type="password"
+        onChange={(e) => {
+          console.log(`Password: ${e.target.value}`);
+        }}
+        name="password"
+      />
+      <br />
+      <button>Login</button>
+    </form>
+  );
+}
