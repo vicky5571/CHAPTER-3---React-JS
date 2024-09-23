3dcba6daad11b27bde47ffac8a3d818193df04f2
vicky5571 <vickygp5571@gmail.com>
2024-09-23 12:14:15 +0700

    Window event

--- 2. (Anson Developer) React JS Full Course 2024/src/App.jsx
+++ 2. (Anson Developer) React JS Full Course 2024/src/App.jsx
@@ -1,6 +1,10 @@
 import { LoginForm } from "./components/LoginForm";
 
 export default function App() {
+  window.addEventListener("resize", (e) => {
+    console.log(window.innerHeight, window.innerWidth);
+  });
+
   return (
     <div>
       <LoginForm />
