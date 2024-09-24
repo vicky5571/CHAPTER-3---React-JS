da733f3337e964749d0cf5100906a880193e1ce9
vicky5571 <vickygp5571@gmail.com>
2024-09-24 16:03:30 +0700

    Adding elements to State Array

--- 2. (Anson Developer) React JS Full Course 2024/src/App.jsx
+++ 2. (Anson Developer) React JS Full Course 2024/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { UserDetails } from "./components/UserDetails";
 
 export default function App() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [counter, setCounter] = useState(3);
   const [users, setUsers] = useState([
     {
       id: 1,
@@ -17,6 +20,48 @@ export default function App() {
 
   return (
     <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const newUser = {
+            id: counter,
+            username,
+            email,
+          };
+          setCounter((currentCounter) => currentCounter + 1);
+          setUsers((currentUsersState) => [...currentUsersState, newUser]);
+        }}
+      >
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input
+            name="username"
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email: </label>
+          <input
+            name="email"
+            type="email"
+            id="username"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+        <button>Add User</button>
+      </form>
+      <br />
+      <br />
+      <br />
       {users.map((user) => (
         <UserDetails key={user.id} user={user} setUsers={setUsers} />
       ))}
